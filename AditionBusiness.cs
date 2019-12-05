d6fa003c31ce8097a200eb8dd7ca2b298134fece
erramire <erramire@microsoft.com>
2019-12-05 17:19:07 -0600

    Creacion de clase y prueba unitaria de adicion

---
+++ AditionBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace consoleapp1
+{
+    public class AditionBusiness
+    {
+        public int Add(int a, int b) {
+            int result = 0;
+            result = a + b;
+            return result;
+        }
+    }
+}
